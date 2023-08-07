71135dd16f7cf707205cd638420e268047274cea
Lucas Becker <lks.becker@gmail.com>
2023-08-07 15:05:17 -0300

    starting project

---
+++ src/main/kotlin/com/lucbecker/wwc/WomensWorldCup2023Application.kt
@@ -0,0 +1,11 @@
+package com.lucbecker.womensworldcup2023
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WomensWorldCup2023Application
+
+fun main(args: Array<String>) {
+	runApplication<WomensWorldCup2023Application>(*args)
+}
